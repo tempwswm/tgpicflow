da696813f31861a5cc49458b01cf8838692cfc59
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-02-27 00:03:29 +0800

    运行版本提交
    就酱紫

---
+++ src/share.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+import Cookies from 'js-cookie'
+function setrmad(){Cookies.set('rmads', '1', { expires: 7 })}
+
+const aaurl = ref("/ads/share2.png")
+</script>
+
+<template>
+  <img alt="二维码" :src="aaurl" style="max-width: 80%; max-height: 80%;"/>
+  <h2><a :href="aaurl" v-on:click="setrmad" style="font-size: 3em;" download>保存分享</a></h2>
+</template>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base:"./",
+  build: {
+     rollupOptions: {
+       input: {
+         main: resolve(__dirname, 'index.html'),
+         share: resolve(__dirname, 'share.html')
+       }
+     }
+   }
+})
